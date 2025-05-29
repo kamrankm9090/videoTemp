03e513d91c5742f2066183c2a78e3cab1774db14
kamranmohamadi <kamran.km9090@gmail.com>
2025-05-29 18:39:10 +0330

    fix merge profile

--- src/components/molecules/UserIdentityHeader/index.tsx
+++ src/components/molecules/UserIdentityHeader/index.tsx
@@ -2,6 +2,7 @@ import {Avatar} from '@rneui/base';
 import React, {memo} from 'react';
 import {PencilIcon} from '~/assets/svgs';
 import {AppText, AppTouchable, HStack, VStack} from '~/components';
+import {navigate} from '~/navigation/methods';
 import {Colors} from '~/styles';
 import {fontFamily, fontSize, scale} from '~/utils/style';
 
@@ -31,17 +32,43 @@ const UserIdentityHeader = () => {
         </AppTouchable>
       </HStack>
       <HStack justifyContent="space-between" alignItems="center">
-        <Item title="balance" value={'$200'} />
-        <Item title="Followers" value={'343'} />
-        <Item title="Following" value={'200'} />
+        <Item title="balance" value={'$200'} onPress={() => null} />
+        <Item
+          title="Followers"
+          value={'343'}
+          onPress={() =>
+            navigate('ProfileStack', {
+              screen: 'FollowerFollowing',
+              params: {type: 'follower'},
+            })
+          }
+        />
+        <Item
+          title="Following"
+          value={'200'}
+          onPress={() =>
+            navigate('ProfileStack', {
+              screen: 'FollowerFollowing',
+              params: {type: 'following'},
+            })
+          }
+        />
       </HStack>
     </VStack>
   );
 };
 
-const Item = ({title, value}: {title: string; value: any}) => {
+const Item = ({
+  title,
+  value,
+  onPress,
+}: {
+  title: string;
+  value: any;
+  onPress: () => void;
+}) => {
   return (
-    <AppTouchable>
+    <AppTouchable onPress={onPress}>
       <VStack alignItems="center" space={scale(5)} justifyContent="center">
         <AppText fontSize={fontSize.xMedium} fontFamily={fontFamily.bold}>
           {value}
